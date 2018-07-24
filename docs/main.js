f79725efbe8cc22436730df451bf506eaeaa5d4f
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-24 17:37:28 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,27 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+let stream = null;
+function appendVideo(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+    video.play();
+}
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+    console.log(`streamId:${stream.id}`);
+    appendVideo(stream);
+    const peer = new Peer(myId, {
+        key: '01099bd8-1083-4c33-ba9b-564a1377e901',
+        turn: true
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            appendVideo(stream);
+        });
+    });
+}).catch(err => {
+    console.error(err);
+});
